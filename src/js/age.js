67a9071901954a97a971a0eb4b5ca42f0ddeba56
Student <example@example.com>
2018-10-05 10:57:14 -0700

    pass venus test

--- src/js/age.js
+++ src/js/age.js
@@ -12,3 +12,8 @@ Age.prototype.mercury = function(){
   let mercuryAge = this.userAge * .24;
   return mercuryAge;
 }
+
+Age.prototype.venus = function(){
+  let venusAge = this.userAge * .62;
+  return venusAge;
+}
